2d0154c2848165b3b699efe0de1a0fd00969b413
Juan Porto <toto_lp77@hotmail.com>
2020-01-30 12:33:06 -0300

    Starting parse logic on content manager and SearchResultParser class

--- src/content_scripts/main.js
+++ src/content_scripts/main.js
@@ -1,3 +1,29 @@
+class SearchResultParser {
+  constructor(dom){
+    var host = window.location.hostname;
+    if (host == 'duckduckgo.com'){
+      var titulos = new Array();
+      // var resultados = dom.getElementById("links").querySelectorAll("div:not(#organic-module):not(.result):not(.result--more)");
+      var resultados = dom.getElementById("links").childNodes;
+
+      for (var i = 0; i < resultados.length; i++) {
+        if (resultados[i].id != "organic-module" && resultados[i].className != "result result--more" && resultados[i].className != "js-result-hidden-el"){
+          titulos.push(resultados[i]);
+        }
+      }
+      console.log(titulos);
+      // resultados.forEach(resultado => {
+      //   titulos.push(resultado.querySelector(".result__title").querySelector(".result__a").innerHTML);
+      // });
+    }
+    this.urlsrc = 'hola';
+    this.urltarget = 'hola';
+    this.text = titulos;
+    this.rank = 'hola';
+  }
+
+}
+
 class ContentPageManager{
   extractSearchString(args){
     var searchString = document.getElementsByName("q")[0].value;
@@ -12,8 +38,8 @@ class ContentPageManager{
   }
 
   extractSearchResults(){
-    alert("hola, desde extractSearchResults");
-    console.log("mierrr");
+    var result = new SearchResultParser(document);
+    alert(result.text);
     // chrome.runtime.sendMessage({
     //   "call": "decirHola"
     // });
