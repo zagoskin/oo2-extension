5d012ac142a6c645ee64c88c90c398ef6f6e4b6b
Juan Porto <toto_lp77@hotmail.com>
2020-01-31 18:18:45 -0300

    SearchResultParser, parseFromBing & parseFromGoogle (moved duckduckgo logic to method)

--- src/content_scripts/main.js
+++ src/content_scripts/main.js
@@ -1,27 +1,73 @@
 class SearchResultParser {
   constructor(dom){
     var host = window.location.hostname;
-    if (host == 'duckduckgo.com'){
-      var parsedResults = new Array();
-      // var resultados = dom.getElementById("links").querySelectorAll("div:not(#organic-module):not(.result):not(.result--more)");
-      var divResults = dom.getElementById("links").childNodes;
 
-      for (var i = 0; i < divResults.length; i++) {
-        if (divResults[i].id != "organic-module" && divResults[i].className != "result result--more" && divResults[i].className != "js-result-hidden-el"){
-          parsedResults.push({
-            "urlsrc": host,
-            "urltarget": divResults[i].querySelector(".result__title").querySelector(".result__a").href,
-            "text": divResults[i].querySelector(".result__title").innerText,
-            "rank": i
-          });
-        }
-      }
-      console.log(parsedResults);
-      // resultados.forEach(resultado => {
-      //   titulos.push(resultado.querySelector(".result__title").querySelector(".result__a").innerHTML);
-      // });
+    if (host == 'duckduckgo.com'){
+      this.results = this.parseFromDuckDuckGo(dom);
     }
-    this.results = parsedResults;
+
+    if (host == 'www.bing.com'){
+      this.results = this.parseFromBing(dom);
+    }
+
+    if (host == 'www.google.com'){
+      this.results = this.parseFromGoogle(dom);
+    }
+    console.log(this.results);
+
+
+
+  }
+
+  parseFromDuckDuckGo(dom){
+    var parsedResults = new Array();
+    // var resultados = dom.getElementById("links").querySelectorAll("div:not(#organic-module):not(.result):not(.result--more)");
+    var ads = dom.querySelector("div.results--main").querySelectorAll(".result__title");
+
+    for (var i = 0; i < ads.length; i++) {
+      parsedResults.push({
+        "urlsrc": 'duckduckgo.com',
+        "urltarget": ads[i].querySelector(".result__a").href,
+        "text": ads[i].textContent,
+        "rank": parsedResults.length
+      });
+    }
+
+    return parsedResults;
+  }
+
+  parseFromBing(dom){
+    var parsedResults = new Array();
+
+    var divResults = dom.getElementById("b_results").childNodes;
+    for (var i = 0; i < divResults.length; i++) {
+      if (divResults[i].className != "b_pag" && divResults[i].className != "b_ans"){
+        parsedResults.push({
+          "urlsrc": 'www.bing.com',
+          "urltarget": divResults[i].querySelector("a").href,
+          "text": divResults[i].querySelector("a").textContent,
+          "rank": parsedResults.length
+        });
+      }
+    }
+
+    return parsedResults;
+  }
+
+  parseFromGoogle(dom){
+    var parsedResults = new Array();
+
+    var divResults = dom.getElementById("search").querySelectorAll("div.r");
+    for (var i = 0; i < divResults.length; i++) {
+      parsedResults.push({
+        "urlsrc": 'www.google.com',
+        "urltarget": divResults[i].querySelector("a").href,
+        "text": divResults[i].querySelector("a").querySelector("h3").textContent,
+        "rank": parsedResults.length
+      });
+    }
+
+    return parsedResults;
   }
 
 }
