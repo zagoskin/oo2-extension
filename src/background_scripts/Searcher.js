26258a68920ddc7c15af627e8b550d30feb7c34a
Juan Porto <toto_lp77@hotmail.com>
2020-02-21 11:05:34 -0300

    Moved class implementation to own file, imported in manifest

--- src/background_scripts/Searcher.js
+++ src/background_scripts/Searcher.js
@@ -1,36 +1,42 @@
 class Searcher {
   constructor(){
     this.hostnames =  new Array("www.google.com", "www.bing.com", "duckduckgo.com");
-    this.results = [];
-  }
-
-  get results (){
-    return this.results;
-  }
-
-  get hostnames (){
-    return this.hostnames;
   }
 
   searchKeyword (keyword){
-    var oReq = new XMLHttpRequest();
-    oReq.onload = function(e) {
-      oReq.onreadystatechange = function() {
-        if (oReq.readystate == XMLHttpRequest.DONE){
-          this.results.push(oReq.responseText);
-          console.log(this.results());
-        }
-      };
-      for (var i = 0; i < this.hostnames.length; i++){
-        if (this.hostnames[i] == "duckduckgo.com") {
-          oReq.open("GET", this.hostnames[i] + "/?q=" + keyword);
-          oReq.send();
-        }
-        else {
-          oReq.open("GET", this.hostnames[i] + "/search?q=" + keyword);
-          oReq.send();
-        }
+
+    var searchurls = new Array();
+    this.hostnames.forEach(function(hostname){
+
+      if (hostname == "duckduckgo.com") {
+        var url ='https://' + hostname + '/?q=' + keyword;
+        searchurls.push(url)
       }
-    }
+      else {
+        var url ='https://' + hostname + '/search?q=' + keyword;
+        searchurls.push(url)
+      }
+    });
+    return searchurls;
+  }
+
+  searchWithoutDomain (keyword,domain){
+
+    var searchurls = new Array();
+    var newHosts = this.hostnames.slice();
+    newHosts.splice(newHosts.indexOf(domain),1);
+
+    newHosts.forEach(function(hostname){
+
+      if (hostname == "duckduckgo.com") {
+        var url ='https://' + hostname + '/?q=' + keyword;
+        searchurls.push(url)
+      }
+      else {
+        var url ='https://' + hostname + '/search?q=' + keyword;
+        searchurls.push(url)
+      }
+    });
+    return searchurls;
   }
 }
---
+++ src/content_scripts/Result.js
@@ -0,0 +1,8 @@
+class Result {
+  constructor(urlsrc,urltarget,text,rank){
+    this.urlsrc = urlsrc;
+    this.urltarget = urltarget;
+    this.text = text;
+    this.rank = rank;
+  }
+}
